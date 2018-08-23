804c28b37749a0c8decb7358f0ac1a056fbf1d45
Ishanz23 <sayantandey23@gmail.com>
2018-08-23 11:52:53 +0530

    prod setup

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,2 @@
-module.exports = {
-  googleClientID:
-    '280841046469-h8cr81tknivh1jevloqr6vodn0h0unps.apps.googleusercontent.com',
-  googleClientSecret: 'w3g9dWpQ3GgH-swj09IO3DxS',
-  mongoURI: 'mongodb://localhost:27017/EmailyDev',
-  cookieKey: 'hkjashdajkbaksrfqiofqwbfjkaufgiqfqrnasf'
-}
+module.exports =
+  process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev')
